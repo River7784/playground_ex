5a49383d85b55f270c2d2548fcc0412d38e4b135
River7784 <119822951+River7784@users.noreply.github.com>
2024-01-27 17:12:36 +0900

    Add files via upload

---
+++ 36.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+//MARK: - map함수는 컨테이너 내부의 기존 데이터를 변형하여 새로운 컨테이너를 생성함
+let numbers: [Int] = [0, 1, 2, 3, 4]
+var doubledNumbers: [Int]
+var strings: [String]
+
+//기존의 for구문 사용
+doubledNumbers = [Int]()
+strings = [String]()
+
+for number in numbers {
+    doubledNumbers.append(number * 2)
+    strings.append("\(number)")
+}
+
+print(doubledNumbers)
+print(strings)
+
+
+// numbers의 각 요소를 2배하여 새로운 배열 반환
+doubledNumbers = numbers.map({ (number: Int) -> Int in
+    return number * 2
+})
+
+// numbers의 각 요소를 문자열로 변환하여 새로운 배열 반환
+strings = numbers.map({ (number: Int) -> String in
+    return "\(number)"
+})
+
+print(doubledNumbers) // [0, 2, 4, 6, 8]
+print(strings) // ["0", "1", "2", "3", "4"]
+
+// 매개변수, 반환 타입, 반환 키워드(return) 생략, 후행 클로저
+doubledNumbers = numbers.map { $0 * 2 }
+print(doubledNumbers) // [0, 2, 4, 6, 8]
