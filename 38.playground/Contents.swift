db454bc3ec04a41a967f3462dd7c1209eee7dfbe
River7784 <119822951+River7784@users.noreply.github.com>
2024-02-05 17:37:38 +0900

    Add files via upload

--- 38.playground/Contents.swift
+++ 38.playground/Contents.swift
@@ -45,3 +45,48 @@ extension Sendable {
         return false
     }
 }
+
+// 수신, 발신이 가능한 Message 클래스
+class Message: Sendable, Receiveable {
+    var to: Receiveable?
+}
+
+// 수신, 발신이 가능한 Mail 클래스
+class Mail: Sendable, Receiveable {
+    var to: Receiveable?
+}
+
+// 두 Message 인스턴스를 생성합니다.
+let myPhoneMessage: Message = Message()
+let yourPhoneMesssage: Message = Message()
+
+// 아직 수신받을 인스턴스가 없습니다.
+myPhoneMessage.send(data: "Hello") // Message has no receiver
+
+// Message 인스턴스는 발신과 수신이 모두 가능하므로 메시지를 주고 받을 수 있습니다.
+myPhoneMessage.to = yourPhoneMesssage
+myPhoneMessage.send(data: "Hello") // Message received Hello from Message
+
+// Mail 인스턴스를 두 개 생성합니다.
+let myMail: Mail = Mail()
+let yourMail: Mail = Mail()
+
+myMail.send(data: "Hi") // Mail has no receiver
+
+// Message와 Mail 모두 Sendable과 Receiveable 프로토콜을 준수하므로 서로 주고 받을 수 있습니다.
+myMail.to = yourMail
+myMail.send(data: "Hi") // Mail received Hi from Mail
+
+myMail.to = myPhoneMessage
+myMail.send(data: "Bye") // Message received Bye from Mail
+
+// String은 Sendable 프로토콜을 준수하지 않습니다.
+Message.isSendableInstance("Hello") // false
+
+// Message와 Mail은 Sendable 프로토콜을 준수합니다.
+Message.isSendableInstance(myPhoneMessage) // true
+
+// yourPhoneMessage는 to 프로퍼티가 설정되지 않아서 보낼 수 없는 상태입니다.
+Message.isSendableInstance(yourPhoneMesssage) // false
+Mail.isSendableInstance(myPhoneMessage) // true
+Mail.isSendableInstance(myMail) // true
